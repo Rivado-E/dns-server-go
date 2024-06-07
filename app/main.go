5c1b32fcb266f7900a00e0c45f68b41a45954328
Rivado Edah <redah@umd.edu>
2024-06-07 02:10:33 -0400

    attempt at phase 2

--- app/main.go
+++ app/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 )
+
+type DnsMsg struct {
+	ID      int
+	Message []byte
+}
+
+func (msg *DnsMsg) toResponse(response *bytes.Buffer) {
+	err := binary.Write(response, binary.BigEndian, uint16(msg.ID))
+	if err != nil {
+		fmt.Println("Unable to construct id for the header: ", err)
+	}
+
+	if _, err := response.Write(msg.Message); err != nil {
+		fmt.Println("Unable to add the response: ", err)
+	}
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -36,11 +54,15 @@ func main() {
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
 		// Create an empty response
-		response := []byte{}
+		response := new(bytes.Buffer)
+		msg := DnsMsg{1234, []byte{0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}
 
-		_, err = udpConn.WriteToUDP(response, source)
+		msg.toResponse(response)
+
+		_, err = udpConn.WriteToUDP(response.Bytes(), source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
 	}
 }
+
