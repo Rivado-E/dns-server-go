dd92ffe370a862fe8ec48b1d52c1345f38e1fa9b
rivaldoe <redah@umd.edu>
2024-07-06 00:40:02 -0400

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,34 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
-	// "fmt"
 	"log"
 	"net"
+
+	dns "github.com/codecrafters-io/dns-server-starter-go/lib"
 )
 
-type DnsMsg struct {
-	ID      int
-	Message []byte
-}
-
-func (msg *DnsMsg) toResponse(response *bytes.Buffer) {
-	err := binary.Write(response, binary.BigEndian, uint16(msg.ID))
-	if err != nil {
-		log.Println("Unable to construct id for the header: ", err)
-	}
-
-	if _, err := response.Write(msg.Message); err != nil {
-		log.Println("Unable to add the response: ", err)
-	}
-}
-
 func dnsServer() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	// udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:4200")
 	if err != nil {
@@ -54,16 +33,34 @@ func dnsServer() {
 			break
 		}
 
-		receivedData := string(buf[:size])
-		log.Printf("Received %d bytes from %s : %s\n", size, source, receivedData)
+		message := buf[:size]
+		log.Println("Received %d bytes from %s:\n", size, source)
+		// dns.PrintMessage(message)
 
+		response := []byte{}
+
+		if receivedHeader, receivedQuestions, err := dns.ParseDNSMessage(message); err != nil {
+			log.Fatal(err)
+		} else {
+			headerFlags := dns.DecodeDNSFlags(receivedHeader.Flags)
+			headerFlags.QR = 1
+
+			receivedHeader.QDCount = 1
+			receivedHeader.ID = 1234
+			receivedHeader.Flags = dns.EncodeDNSFlags(headerFlags)
+
+			for i := range receivedQuestions {
+				receivedQuestions[i].QType = 1
+				receivedQuestions[i].QClass = 1
+			}
+
+			response = dns.EncodeDNSMessage(receivedHeader, receivedQuestions)
+			// log.Println("Here is my response")
+			// dns.PrintMessage(response)
+		}
 		// Create an empty response
-		response := new(bytes.Buffer)
-		msg := DnsMsg{1234, []byte{0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}
 
-		msg.toResponse(response)
-
-		_, err = udpConn.WriteToUDP(response.Bytes(), source)
+		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			log.Println("Failed to send response:", err)
 		}
